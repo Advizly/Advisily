ce7d3ef0d43aa5d4015e9828633b6034f883fd97
Youssef Agiza <youssefagiza@aucegypt.edu>
2021-06-14 20:13:30 +0200

    switches frontend from angular to react

--- discarded/frontendAngular/AutomatedAdvising/src/app/app.module.ts
+++ discarded/frontendAngular/AutomatedAdvising/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
