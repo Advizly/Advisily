3df4e9a9b129c3bde4a98ee3107483a870163a21
Youssef Agiza <youssefagiza@aucegypt.edu>
2021-08-07 05:56:20 +0200

    minor edits in backend

--- advisily-api/index.js
+++ advisily-api/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const home = require("./routes/home");
+const plans = require("./routes/plans");
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use("/", home);
+app.use("/api/plans", plans);
 
 app.listen(port, () => console.log(`App listening on port ${port}....`));
--- advisily-api/routes/home.js
+++ advisily-api/routes/home.js
@@ -9,15 +9,27 @@ const getConnection = () =>
     password: process.env.DB_PASSWORD,
     database: "advisily",
   });
+
 router.get("/", (req, res) => {
   const connection = getConnection();
-  connection.query("SELECT * FROM template_courses", (err, results, fields) => {
+  const query =
+    "select c.course_code, c.title, c.department_id, semester_number,catalog_year,d.prefix\
+	    	from plan_courses AS pc JOIN courses AS c \
+        ON (pc.course_code=c.course_code AND pc.course_department_id=c.department_id)\
+        JOIN departments as d ON(c.department_id=d.department_id) \
+        WHERE  catalog_year= ?\
+        ORDER BY semester_number ";
+  connection.query(query, [2021], (err, results, fields) => {
     if (err) {
       console.log("Error in quyring data", err);
       return res.send("Error");
     }
+    results.forEach((r, index, arr) => {
+      arr[index] = r.course_code < 0 ? { ...r, course_code: "XXX" } : r;
+      console.log("R is:", arr[index]);
+    });
     res.send(results);
-    console.log(results);
+    // console.log(results);
     connection.end();
   });
 });
